0d241f5a58bb2ba7271900bd5194d69c0cea3e4c
MarcoSorci <97511096+MarcoSorci@users.noreply.github.com>
2022-02-22 12:23:21 +0100

    buttonz

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 
 function createChess(size) {
     const board = document.getElementById('main-div')
+    const side = 50 / size;
     for (let y = 0; y < size; y++) {
         let row = document.createElement('div')
         row.id = 'row'
         for (let x = 0; x < size; x++) {
             if ((x + y) % 2 == 0) {
                 const boardblack = document.createElement('div')
+                boardblack.style.height = side + 'vw';       //vh/vw is view height/width, aka the full measurements of the browser window
+                boardblack.style.width = side + 'vw';
                 boardblack.className += 'div-black'
                 row.appendChild(boardblack)
             } else {
                 const boardwhite = document.createElement('div')
                 boardwhite.className += 'div-white'
+                boardwhite.style.height = side + 'vw';
+                boardwhite.style.width = side + 'vw';
                 row.appendChild(boardwhite)
             }
         }
